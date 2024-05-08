cdeab7e194e2ce9f60321b349c36f6fdbb0a037a
Hometasks <vaibhav@vedspace.in>
2024-05-08 17:25:47 +0530

    Education Field
    Education Field

--- users/models.py
+++ users/models.py
@@ -2,10 +2,20 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
+
+    DEGREE_CHOICES = [
+        ('BTECH', 'B.Tech'),
+        ('BCA', 'BCA'),
+        ('BBA', 'BBA'),
+        ('MCA', 'MCA'),
+        ('MBA', 'MBA'),
+        ('BSC', 'B.Sc.'),
+        ('BDES', 'B.Des'),
+    ]
     photo = models.ImageField(upload_to='user_photos/', null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
     skills = models.CharField(max_length=255, null=True, blank=True)
-    education = models.CharField(max_length=255, null=True, blank=True)
+    education = models.CharField(max_length=50, choices=DEGREE_CHOICES, null=True, blank=True)
     college = models.CharField(max_length=255, null=True, blank=True)
     age = models.IntegerField(null=True, blank=True)
     gender = models.CharField(max_length=10, null=True, blank=True)
