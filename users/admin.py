57d40232e98eaf567aa9d3d311888e10a699fdc2
Hometasks <vaibhav@vedspace.in>
2024-05-09 22:17:28 +0530

    Separate Custom Users
    Custom Users

--- users/admin.py
+++ users/admin.py
@@ -1,17 +1,115 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django import forms
+from django.core.exceptions import ValidationError
+from .models import CustomUser, CompanyUser, CollegeUser
 
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('photo', 'bio', 'skills', 'education', 'college', 'age', 'gender', 'phone_number', 'instagram_profile', 'linkedin_profile', 'github_profile')}),
-    )
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        (None, {'fields': ('photo', 'bio', 'skills', 'education', 'college', 'age', 'gender', 'phone_number', 'instagram_profile', 'linkedin_profile', 'github_profile')}),
-    )
-    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff', 'college', 'phone_number', 'instagram_profile', 'linkedin_profile', 'github_profile']
-    search_fields = ['username', 'first_name', 'last_name', 'email', 'skills', 'college', 'instagram_profile', 'linkedin_profile', 'github_profile']
-    list_filter = UserAdmin.list_filter + ('college', 'skills',)
+# Custom Forms for Creating and Changing Users
 
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'password', 'is_active', 'is_admin')
+
+    def clean_password(self):
+        return self.initial["password"]
+
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password Confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = CustomUser
+        fields = ('email',)
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+# UserAdmin classes for each user type
+
+class CustomUserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    list_display = ('email', 'is_admin', 'is_active')
+    list_filter = ('is_admin', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('photo', 'bio', 'skills', 'education', 'college', 'age', 'gender', 'phone_number', 'instagram_profile', 'linkedin_profile', 'github_profile')}),
+        ('Permissions', {'fields': ('is_admin', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_active', 'is_admin'),
+        }),
+        ('Personal Info', {
+            'classes': ('wide',),
+            'fields': ('photo', 'bio', 'skills', 'education', 'college', 'age', 'gender', 'phone_number', 'instagram_profile', 'linkedin_profile', 'github_profile'),
+        }),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+# Repeat similar setups for CompanyUser and CollegeUser if they have unique fields or requirements
+
+class CompanyUserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    model = CompanyUser
+    list_display = ('email', 'company_name', 'is_active')
+    list_filter = ('is_active',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Company Information', {'fields': ('company_name', 'website', 'description', 'employees', 'category', 'contact_number')}),
+        ('Permissions', {'fields': ('is_active',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'company_name', 'website', 'description', 'employees', 'category', 'contact_number', 'is_active'),
+        }),
+    )
+    search_fields = ('email', 'company_name')
+    ordering = ('email',)
+    filter_horizontal = ()
+
+class CollegeUserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    model = CollegeUser
+    list_display = ('email', 'college_name', 'is_active')
+    list_filter = ('is_active',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('College Information', {'fields': ('college_name', 'courses', 'tnp_cell_contact', 'category', 'number_of_students', 'social_profile')}),
+        ('Permissions', {'fields': ('is_active',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'college_name', 'courses', 'tnp_cell_contact', 'category', 'number_of_students', 'social_profile', 'is_active'),
+        }),
+    )
+    search_fields = ('email', 'college_name')
+    ordering = ('email',)
+    filter_horizontal = ()
+
+# Register models with their respective admins
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(CompanyUser, CompanyUserAdmin)
+admin.site.register(CollegeUser, CollegeUserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,8 +1,27 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class CustomUser(AbstractUser):
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db import models
 
+# Manager for CustomUser
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        user = self.create_user(email, password=password, **extra_fields)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+class CustomUser(AbstractBaseUser):
     DEGREE_CHOICES = [
         ('BTECH', 'B.Tech'),
         ('BCA', 'BCA'),
@@ -11,6 +30,7 @@ class CustomUser(AbstractUser):
         ('BSC', 'B.Sc.'),
         ('BDES', 'B.Des'),
     ]
+    email = models.EmailField(max_length=255, unique=True, null=True, blank=True)
     photo = models.ImageField(upload_to='user_photos/', null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
     skills = models.CharField(max_length=255, null=True, blank=True)
@@ -22,3 +42,61 @@ class CustomUser(AbstractUser):
     instagram_profile = models.URLField(null=True, blank=True)
     linkedin_profile = models.URLField(null=True, blank=True)
     github_profile = models.URLField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+# Separate CompanyUser and CollegeUser models with similar attributes as CustomUser but managed separately
+class CompanyUserManager(BaseUserManager):
+    # Implementation similar to CustomUserManager
+    pass
+
+class CompanyUser(AbstractBaseUser):
+    company_name = models.CharField(max_length=255)
+    website = models.URLField(max_length=255)
+    description = models.TextField()
+    employees = models.IntegerField()
+    category = models.CharField(max_length=100)
+    contact_number = models.CharField(max_length=15)
+    email = models.EmailField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)  # Make sure this is defined
+    is_admin = models.BooleanField(default=False)  # Administrative access to Django admin
+
+    USERNAME_FIELD = 'email'
+    objects = CompanyUserManager()
+
+class CollegeUserManager(BaseUserManager):
+    # Implementation similar to CustomUserManager
+    pass
+
+class CollegeUser(AbstractBaseUser):
+    college_name = models.CharField(max_length=255)
+    courses = models.TextField()
+    tnp_cell_contact = models.CharField(max_length=255)
+    category = models.CharField(max_length=100)
+    number_of_students = models.IntegerField()
+    social_profile = models.URLField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)  # Make sure this is defined
+    is_admin = models.BooleanField(default=False)  # Administrative access to Django admin
+
+    USERNAME_FIELD = 'email'
+    objects = CollegeUserManager()
